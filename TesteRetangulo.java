8bada097cff3bcfcc08911185088adc1b8849cec
Marcel0Maia <123846130+Marcel0Maia@users.noreply.github.com>
2023-06-04 20:51:08 -0300

    Update TesteRetangulo.java

--- TesteRetangulo.java
+++ TesteRetangulo.java
@@ -1,12 +1,27 @@
+import java.util.Scanner;
 
 public class TesteRetangulo {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
 		Retangulo[] ret = new Retangulo[2];
 		
-		ret[0] = new Retangulo(30, 20);
-		ret[1] = new Retangulo(33, 35);
+	System.out.println("Escreva os valores que deseja settar para o retangulo 1");
+		int comprimento = sc.nextInt();
+		int largura = sc.nextInt();
+		ret[0] = new Retangulo(comprimento, largura);
+		
+		
+		
+		
+		
+		System.out.println("Escreva os valores que deseja settar para o retangulo 2");
+		comprimento = sc.nextInt();
+		largura = sc.nextInt();
+		ret[1] = new Retangulo(comprimento, largura);
+		
 		
 		System.out.println(ret[0].toString());
 		System.out.println(ret[1].toString());
+		sc.close();
 	}
 }
